93c1af704be808d02de00438d755bb99e489cf48
zbdwin <812114007@qq.com>
2016-08-23 20:57:02 +0800

    zbd version1.3

---
+++ library/src/main/java/com/handmark/pulltorefresh/library/internal/FrameAnimationLayout.java
@@ -0,0 +1,52 @@
+package com.handmark.pulltorefresh.library.internal;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.drawable.AnimationDrawable;
+import android.graphics.drawable.Drawable;
+
+import com.handmark.pulltorefresh.library.PullToRefreshBase;
+import com.bwf.tuanche.R;
+/**
+ * Created by admin on 2016/8/23.
+ */
+public class FrameAnimationLayout extends LoadingLayout {
+
+    private AnimationDrawable mAnimationDrawable;
+
+    public FrameAnimationLayout(Context context, PullToRefreshBase.Mode mode, PullToRefreshBase.Orientation scrollDirection, TypedArray attrs) {
+        super(context, mode, scrollDirection, attrs);
+        mHeaderImage.setImageResource(R.drawable.show_updata);
+        mAnimationDrawable = (AnimationDrawable) mHeaderImage.getDrawable();
+    }
+
+    @Override
+    protected int getDefaultDrawableResId() {
+        return R.drawable.default_ptr_flip;
+    }
+
+    @Override
+    protected void onLoadingDrawableSet(Drawable imageDrawable) {
+    }
+
+    @Override
+    protected void onPullImpl(float scaleOfLayout) {
+    }
+
+    @Override
+    protected void pullToRefreshImpl() {
+    }
+
+    @Override
+    protected void refreshingImpl() {
+        mAnimationDrawable.start();
+    }
+
+    @Override
+    protected void releaseToRefreshImpl() {
+    }
+
+    @Override
+    protected void resetImpl() {
+    }
+}
